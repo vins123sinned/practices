42c6d2b8ef6b9fc75e480dd6fc56681a80ddb9fe
Vinson 2 <vinsonhe2@gmail.com>
2025-03-29 00:06:20 -0700

    Add HTML and CSS for dropdown and its menu
    Also moved to its own folder and added initial JavaScript functionality

---
+++ dynamic-ui-interactions/dropdown-menu/index.js
@@ -0,0 +1,11 @@
+(function buttonListener() {
+    const dropdownButton = document.querySelector('.dropdown-button');
+
+    
+})();
+
+(function hideButton() {
+    const dropdownContent = document.querySelector('.dropdown-content');
+
+    dropdownContent.classList.add('hidden');
+})();
