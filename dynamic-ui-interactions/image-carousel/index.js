206f10de8f1bc10a3d590cafa25bd49644b81a69
Vinson 2 <vinsonhe2@gmail.com>
2025-03-29 19:32:34 -0700

    Add navigation dots
    Also made moveLeft and moveRight functions into one according to the DRY
    principle.
    Clicking a navigation dot to move there not yet implemented

--- dynamic-ui-interactions/image-carousel/index.js
+++ dynamic-ui-interactions/image-carousel/index.js
@@ -1,8 +1,25 @@
+(function createNavigationDots() {
+    const navigationContainer = document.querySelector('.navigation-container');
+    const images = document.querySelectorAll('img');
+    
+    let position = 0;
+    images.forEach((image) => {
+        const navigationDot = document.createElement('div');
+        navigationDot.dataset.position = `${position}`;
+        navigationDot.classList.add('navigation-dot');
+
+        navigationContainer.appendChild(navigationDot);
+        position += 400;
+    });
+
+    updateNavigationDot(0);
+})();
+
 (function leftArrowListener() {
     const arrowButton = document.querySelector('.arrow-left');
 
     arrowButton.addEventListener('click', () => {
-        moveLeft();
+        moveSlider('left');
     });
 })();
 
@@ -10,30 +27,45 @@
     const arrowButton = document.querySelector('.arrow-right');
 
     arrowButton.addEventListener('click', () => {
-        moveRight();
+        moveSlider('right');
     });
 })();
 
-function moveRight() {
+function moveSlider(direction) {
     const imageSlider = document.querySelector('.image-slider');
     const sliderPosition = window.getComputedStyle(imageSlider).getPropertyValue('left');
+    let newPosition;
 
-    if (sliderPosition === '-2000px') {
-        imageSlider.style.left = '0';
+    if (direction === 'left') {
+        if (sliderPosition === '0px') {
+            newPosition = 2000;
+        } else {
+            newPosition = +sliderPosition.match(/\d+/)[0] - 400;
+        }
     } else {
-        const newPosition = +sliderPosition.match(/\d+/)[0] + 400;
-        imageSlider.style.left = `-${newPosition}px`;
+        if (sliderPosition === '-2000px') {
+            newPosition = 0;
+        } else {
+            newPosition = +sliderPosition.match(/\d+/)[0] + 400;
+        }
     }
+
+    imageSlider.style.left = `-${newPosition}px`;
+    updateNavigationDot(newPosition);
 }
 
-function moveLeft() {
-    const imageSlider = document.querySelector('.image-slider');
-    const sliderPosition = window.getComputedStyle(imageSlider).getPropertyValue('left');
+function updateNavigationDot(position) {
+    const oldNavigationDot = document.querySelector('.current-dot');
+    const navigationDot = document.querySelector(`[data-position='${position}']`);
 
-    if (sliderPosition === '0px') {
-        imageSlider.style.left = '-2000px';
-    } else {
-        const newPosition = +sliderPosition.match(/\d+/)[0] - 400;
-        imageSlider.style.left = `-${newPosition}px`;
-    }
+    if (oldNavigationDot) oldNavigationDot.classList.remove('current-dot');
+    navigationDot.classList.add('current-dot');
 }
+
+function startTimeout() {
+    setTimeout(() => {
+        console.log('a');
+    }, 5000);
+};
+
+startTimeout();
