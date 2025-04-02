49360aaa7ff759e1971e94bc99617b6f62fe8037
Vinson <vinsonhe2@gmail.com>
2025-04-02 10:22:50 -0700

    Add validation for postal code input

--- js-form-validation/index.js
+++ js-form-validation/index.js
@@ -7,7 +7,21 @@
     emailInput.addEventListener('input', () => {
         const emailValidity = isValidEmail(emailInput);
         setEmailClass(emailInput, emailValidity);
-        updateError(emailValidity, emailInput, emailError);
+        updateEmailError(emailValidity, emailInput, emailError);
+    });
+})();
+
+(function postalListener() {
+    const countryInput = document.getElementById('country');
+    const postalInput = document.getElementById('postal-code');
+    const postalError = document.getElementById('postal-error');
+
+    countryInput.addEventListener('change', () => {
+        countryChanged(postalInput, postalError);
+    });
+
+    postalInput.addEventListener('input', () => {
+        checkPostalCode(postalError);
     });
 })();
 
@@ -19,7 +33,18 @@ function isValidEmail(emailInput) {
     return validity;
 }
 
-function updateError(isValidInput, emailInput, emailError) {
+// might add this to all inputs or remove entirely
+function initializeEmailValidation(emailInput) {
+    const emailValidity = isValidEmail(emailInput);
+
+    setEmailClass(emailInput, emailValidity);
+}
+
+function setEmailClass(emailInput, emailValidity) {
+    emailInput.className = emailValidity ? 'valid' : 'invalid';
+}
+
+function updateEmailError(isValidInput, emailInput, emailError) {
     if (isValidInput) {
         emailError.textContent = '';
         emailError.removeAttribute('class');
@@ -33,14 +58,50 @@ function updateError(isValidInput, emailInput, emailError) {
     emailError.setAttribute('class', 'active');
 }
 
-function setEmailClass(emailInput, emailValidity) {
-    emailInput.className = emailValidity ? 'valid' : 'invalid';
+function countryChanged(postalInput, postalError) {
+    if (postalInput.value.length !== 0) {
+        checkPostalCode(postalError)
+    } else if (postalError.innerHTML) {
+        postalError.textContent = '';
+        postalError.removeAttribute('class');
+    }
 }
 
-function initializeEmailValidation(emailInput) {
-    const emailValidity = isValidEmail(emailInput);
+function checkPostalCode(postalError) {
+    const constraints = {
+        us: [
+            '^(US-)?\\d{5}$',
+            'U.S. postal codes must have exactly 5 digits: e.g. US-94027 or 33109',
+        ],
+        cn: [
+            '^(CN-)?\\d{6}$',
+            'Chinese postal codes must have exactly 6 digits: e.g. CN-350316 or 361006',
+        ],
+        is: [
+            '^(IS-)?\\d{3}$',
+            'Icelandic postal codes must have exactly 3 digits: e.g. IS-101 or 220',
+        ],
+        au: [
+            '^(AU-)\\d{4}$',
+            'Australian postal codes must have exactly 4 digits: e.g. AU-0872 or 2750',
+        ],
+        eg: [
+            '^(EG-)\\d{7}$',
+            'Egyptian postal codes must have exactly 7 digits: e.g. EG-3512201 or 4252360',
+        ],
+    };
 
-    setEmailClass(emailInput, emailValidity);
+    const country = document.getElementById('country').value;
+    const postalCodeInput = document.getElementById('postal-code');
+    const constraint = new RegExp(constraints[country][0], '');
+
+    if (constraint.test(postalCodeInput.value)) {
+        postalError.textContent = '';
+        postalError.removeAttribute('class');
+    } else {
+        postalError.textContent = constraints[country][1];
+        postalError.setAttribute('class', 'active');
+    }
 }
 
 /*
@@ -49,6 +110,9 @@ const handleSubmit = (event) => {
 
   const emailInput = isValidEmail();
   setEmailClass(emailInput);
-  updateError(emailInput);
+  updateEmailError(emailInput);
 };
 */
+
+// is it better to do { arrays } or [ objects ]?
+// and what does \\ mean in regex? I'll ask ChatGPT later
